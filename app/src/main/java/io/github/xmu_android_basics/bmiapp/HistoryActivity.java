6ec17b89ebf39d987bea8bd6fb4fc315882711d0
windwood-xmu <windwood@xmu.edu.cn>
2017-07-06 23:45:53 +0800

    实验2.1 添加HistoryActivity，并增加一个全屏的TextView

---
+++ app/src/main/java/io/github/xmu_android_basics/bmiapp/HistoryActivity.java
@@ -0,0 +1,13 @@
+package io.github.xmu_android_basics.bmiapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
